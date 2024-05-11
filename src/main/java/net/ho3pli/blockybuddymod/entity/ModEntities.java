d8b6874466f6623aa00b318e49116263648b6ee6
Ho3pLi <daniele.barile0101@gmail.com>
2024-05-11 16:08:41 +0200

    Update
    Register bug fixed, BBEntity changed from Animal to LivingEntity

--- src/main/java/net/ho3pli/blockybuddymod/entity/ModEntities.java
+++ src/main/java/net/ho3pli/blockybuddymod/entity/ModEntities.java
@@ -1,19 +1,29 @@
 package net.ho3pli.blockybuddymod.entity;
 
 import net.ho3pli.blockybuddymod.BlockyBuddyMod;
+import net.ho3pli.blockybuddymod.entity.client.BB;
 import net.ho3pli.blockybuddymod.entity.custom.BBEntity;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
+import java.util.function.Supplier;
+
 public class ModEntities {
-    public static  final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(NeoForgeRegistries.ENTITY_TYPES, BlockyBuddyMod.MODID);
+    public static  final DeferredRegister<EntityType<?>> entityRegister = DeferredRegister.create(BuiltInRegistries.ENTITY_TYPE, BlockyBuddyMod.MODID);
 
-    public static final RegistryObject<EntityType<BBEntity>> BB = ENTITY_TYPES.register("BB", ()-> EntityType.Builder.of(BBEntity::new, MobCategory.CREATURE).sized(2.5f, 2.5f).build("BB"));
-
+    public static final DeferredHolder<EntityType<?>, EntityType<BBEntity>> entityHolder = entityRegister.register("BB",
+                ()-> EntityType.Builder.<BBEntity>of(BBEntity::new, MobCategory.CREATURE)
+                        .sized(1.0f, 1.0f)
+                        .build(new ResourceLocation(BlockyBuddyMod.MODID, "BB").toString())
+    );
     public static void register(IEventBus eventBus){
-        ENTITY_TYPES.register(eventBus);
+        entityRegister.register(eventBus);
     }
 }
--- src/main/java/net/ho3pli/blockybuddymod/entity/custom/BBEntity.java
+++ src/main/java/net/ho3pli/blockybuddymod/entity/custom/BBEntity.java
@@ -1,8 +1,10 @@
 package net.ho3pli.blockybuddymod.entity.custom;
 
+import net.ho3pli.blockybuddymod.BlockyBuddyMod;
+import net.ho3pli.blockybuddymod.entity.ModEntities;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
@@ -12,33 +14,65 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
-public class BBEntity extends Animal {
+public class BBEntity extends LivingEntity {
+    public BBEntity(EntityType<BBEntity> type, Level level) {
+        super(type, level);
+    }
 
-    public BBEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
-        super(pEntityType, pLevel);
+    public BBEntity(Level level, double x, double y, double z){
+        this(ModEntities.entityHolder.get(), level);
+        setPos(x, y, z);
+    }
+
+    public BBEntity(Level level, BlockPos position){
+        this(level, position.getX(), position.getY(), position.getZ());
     }
 
     @Override
-    public boolean isFood(ItemStack pStack) {
-        return false;
-    }
-
-    @Nullable
-    @Override
-    public AgeableMob getBreedOffspring(ServerLevel pLevel, AgeableMob pOtherParent) {
+    public Iterable<ItemStack> getArmorSlots() {
         return null;
     }
 
     @Override
-    protected void registerGoals() {
-        this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new TryFindWaterGoal(this));
+    public ItemStack getItemBySlot(EquipmentSlot pSlot) {
+        return null;
     }
 
-    public static AttributeSupplier.Builder createAttributes(){
-        return Animal.createLivingAttributes()
-                .add(Attributes.MAX_HEALTH, 20D)
-                .add(Attributes.MOVEMENT_SPEED, 1D)
-                .add(Attributes.ATTACK_DAMAGE, 1D);
+    @Override
+    public void setItemSlot(EquipmentSlot pSlot, ItemStack pStack) {
+
     }
+
+    @Override
+    public HumanoidArm getMainArm() {
+        return null;
+    }
+
+//    public BBEntity(EntityType<? extends Animal> pEntityType, Level pLevel) {
+//        super(pEntityType, pLevel);
+//    }
+//
+//    @Override
+//    public boolean isFood(ItemStack pStack) {
+//        return false;
+//    }
+//
+//    @Nullable
+//    @Override
+//    public AgeableMob getBreedOffspring(ServerLevel pLevel, AgeableMob pOtherParent) {
+//        return null;
+//    }
+//
+//    @Override
+//    protected void registerGoals() {
+//        this.goalSelector.addGoal(0, new FloatGoal(this));
+//        this.goalSelector.addGoal(1, new TryFindWaterGoal(this));
+//    }
+//
+//    public static AttributeSupplier.Builder createAttributes(){
+//        return Animal.createLivingAttributes()
+//                .add(Attributes.MAX_HEALTH, 20D)
+//                .add(Attributes.MOVEMENT_SPEED, 1D)
+//                .add(Attributes.ATTACK_DAMAGE, 1D);
+//    }
 }
