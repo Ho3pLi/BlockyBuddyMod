cb44dee2ad173605240adc744634117d73bd50fb
Ho3pLi <daniele.barile0101@gmail.com>
2024-05-14 16:00:57 +0200

    Update
    refactor

--- src/main/java/com/mylifeblike/blockybuddymod/init/EntityInit.java
+++ src/main/java/com/mylifeblike/blockybuddymod/init/EntityInit.java
@@ -1,21 +1,16 @@
-package net.ho3pli.blockybuddymod.entity;
+package com.mylifeblike.blockybuddymod.init;
 
-import net.ho3pli.blockybuddymod.BlockyBuddyMod;
-import net.ho3pli.blockybuddymod.entity.client.BB;
-import net.ho3pli.blockybuddymod.entity.custom.BBEntity;
+import com.mylifeblike.blockybuddymod.BlockyBuddyMod;
+import com.mylifeblike.blockybuddymod.entity.BBEntity;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
-import java.util.function.Supplier;
-
-public class ModEntities {
+public class EntityInit {
     public static  final DeferredRegister<EntityType<?>> entityRegister = DeferredRegister.create(BuiltInRegistries.ENTITY_TYPE, BlockyBuddyMod.MODID);
 
     public static final DeferredHolder<EntityType<?>, EntityType<BBEntity>> entityHolder = entityRegister.register("BB",
